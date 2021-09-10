f4ea1b3685056cbd4d1f201dbb795a2134bf9b91
Xian <ahx.agent007@gmail.com>
2021-09-10 20:26:32 +0600

    Splash Screen added

--- app/src/main/java/com/secretdevbd/xian/pigeoninfinityapp/SplashScreenActivity.java
+++ app/src/main/java/com/secretdevbd/xian/pigeoninfinityapp/SplashScreenActivity.java
@@ -2,7 +2,9 @@ package com.secretdevbd.xian.pigeoninfinityapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class SplashScreenActivity extends AppCompatActivity {
 
@@ -10,5 +12,18 @@ public class SplashScreenActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+
+        delayLoad();
+    }
+
+    private void delayLoad(){
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            public void run() {
+                // yourMethod();
+                startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                finish();
+            }
+        }, 2000);   //5 seconds
     }
 }
